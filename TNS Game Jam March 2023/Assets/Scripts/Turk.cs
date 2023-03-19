59c78dd5296ebe040d32aead106118aba7283c8c
juberr <zedtrix320@gmail.com>
2023-03-18 21:01:05 -0400

    basic level skeleton, turk additions

--- TNS Game Jam March 2023/Assets/Scripts/Turk.cs
+++ TNS Game Jam March 2023/Assets/Scripts/Turk.cs
@@ -8,11 +8,13 @@ public class Turk : MonoBehaviour
     public float speed;
 
     Rigidbody2D myRB;
+    GameObject myFrontTrigger;
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
         myRB.velocity = new Vector2(speed, 0);
+        myFrontTrigger = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
@@ -24,6 +26,19 @@ public class Turk : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Wall") || collision.CompareTag("HeldShield")) {
             myRB.velocity = new Vector2(-myRB.velocity.x, 0f);
+            myFrontTrigger.transform.localPosition = new Vector3(myFrontTrigger.transform.localPosition.x * -1,
+                myFrontTrigger.transform.localPosition.y,
+                myFrontTrigger.transform.localPosition.z);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground")) {
+            myRB.velocity = new Vector2(-myRB.velocity.x, 0f);
+            myFrontTrigger.transform.localPosition = new Vector3(myFrontTrigger.transform.localPosition.x * -1,
+                myFrontTrigger.transform.localPosition.y,
+                myFrontTrigger.transform.localPosition.z);
         }
     }
 }
